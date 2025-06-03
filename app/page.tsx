ad78f34f3be78b5853ca60050ee0a11637be6c20
Mrosa99 <125097380+Mrosa99@users.noreply.github.com>
2025-06-03 13:45:51 -0400

    Home page and button
    Added more to the home page and create a button from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
 import Image from "next/image";
+import styles from "./page.module.css";
+import { stripe } from "@/lib/stripe";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
-export default function Home() {
-  return <div>Test</div>;
+export default async function Home() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+    limit: 3,
+  });
+  console.log(products);
+  return (
+    <div>
+      <section>
+        <div>
+          <div>
+            <h2>Welcome to my Store</h2>
+            <p>Discover the latest product at best price</p>
+            <Button asChild variant="default">
+              <Link href="/products">Browse All products</Link>
+            </Button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
