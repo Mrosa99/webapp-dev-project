340e0e9503609ae925da0264e21a57821b95a4e4
Mrosa99 <125097380+Mrosa99@users.noreply.github.com>
2025-06-03 15:57:11 -0400

    Added in the CSS for globals for the home page
    Included the styling for divs and sections in the home page. Fixed the wrong oath for the carousel.tsx.

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,46 @@ import styles from "./page.module.css";
 import { stripe } from "@/lib/stripe";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Carousel } from "@/components/carousel";
 
 export default async function Home() {
   const products = await stripe.products.list({
     expand: ["data.default_price"],
     limit: 3,
   });
-  console.log(products);
+
   return (
     <div>
-      <section>
-        <div>
-          <div>
+      <section className="rounded bg-neutral-100 py-8 sm:py-12">
+        <div className="mx-auto grid grid-cols-1 items-center justify-items-center gap-8 px-8 sm:px-16 md:grid-cols-2">
+          <div className="text-3xl font-bold tracking-tight md:text-4xl">
             <h2>Welcome to my Store</h2>
-            <p>Discover the latest product at best price</p>
-            <Button asChild variant="default">
-              <Link href="/products">Browse All products</Link>
+            <p className="text-neutral-600">
+              Discover the latest product at best price
+            </p>
+            <Button
+              asChild
+              variant="default"
+              className="inline-flex items-center justify-center rounded-full px-6 py-3 bg-black text-white"
+            >
+              <Link
+                href="/products"
+                className="inline-flex items-center justify-center rounded-full px-6 py-3"
+              >
+                Browse All products
+              </Link>
             </Button>
           </div>
+          <Image
+            alt="Banner Image"
+            width={450}
+            height={450}
+            src={products.data[0].images[0]}
+          />
         </div>
+      </section>
+      <section className="py-8">
+        <Carousel />
       </section>
     </div>
   );
