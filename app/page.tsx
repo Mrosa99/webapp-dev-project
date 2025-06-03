53751dfb50bddf11996c05547f13b9ef797b3b09
Mrosa99 <125097380+Mrosa99@users.noreply.github.com>
2025-06-03 18:19:54 -0400

    Carousel
    Created the carousel and added it to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default async function Home() {
         </div>
       </section>
       <section className="py-8">
-        <Carousel />
+        <Carousel products={products.data} />
       </section>
     </div>
   );
