be7a3d978c6d5c63ee60cfb03f284e92cd1ba546
MiltonRosa <125097380+Mrosa99@users.noreply.github.com>
2025-06-03 20:48:13 -0400

    Carousel Complete
    Main part of carouselis complete. Small issue with text being same color as background picture but fine for now

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Stripe from "stripe";
-import { Card } from "./ui/card";
+import { Card, CardContent, CardTitle } from "./ui/card";
 import { useEffect, useState } from "react";
 import Image from "next/image";
 
@@ -24,17 +24,26 @@ export const Carousel = ({ products }: Props) => {
   const price = currentProduct.default_price as Stripe.Price;
 
   return (
-    <Card>
+    <Card className="relative overflow-hidden rounded-lg shadow-md border-gray-300">
       {currentProduct.images && currentProduct.images[0] && (
-        <div>
+        <div className="relative h-80 w-full">
           <Image
             alt={currentProduct.name}
             src={currentProduct.images[0]}
             layout="fill"
             objectFit="cover"
+            className="transition-opacity duration-500 ease-in-out"
           />
         </div>
       )}
+      <CardContent className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
+        <CardTitle className="text-3xl font-bold text-black mb-2">
+          {currentProduct.name}
+        </CardTitle>
+        {price && price.unit_amount && (
+          <p>${(price.unit_amount / 100).toFixed(2)}</p>
+        )}
+      </CardContent>
     </Card>
   );
 };
