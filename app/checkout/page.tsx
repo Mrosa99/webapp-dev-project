b80ecf2a91d242058066488e7d98f37ec5e272bd
Mrosa99 <125097380+Mrosa99@users.noreply.github.com>
2025-06-06 17:03:51 -0400

    Checkout Functionality
    Created some of the checkout functionality to display what is being checked out

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,3 +1,44 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useCartStore } from "@/store/cart-store";
+
 export default function CheckoutPage() {
-  return <div>New Checkout</div>;
+  const { items } = useCartStore();
+  const total = items.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  );
+
+  if (total === 0 || items.length === 0) {
+    return (
+      <div>
+        <h1>Your Cart is Empty</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Order Summary</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul>
+            {items.map((items, key) => (
+              <li key={key}>
+                <div>
+                  <span>{items.name}</span>
+                  <span>
+                    ${((items.price * items.quantity) / 100).toFixed(2)}
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
