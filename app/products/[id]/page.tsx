05355df4dabde85252b3971193d16f9d7ef4e60b
MiltonRosa <125097380+Mrosa99@users.noreply.github.com>
2025-06-06 22:17:13 -0400

    Final Build errors

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -4,7 +4,7 @@ import { stripe } from "@/lib/stripe";
 export default async function ProductPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
   const product = await stripe.products.retrieve(id, {
